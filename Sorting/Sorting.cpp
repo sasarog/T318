c566de206c1f8c620196f9c73f1da1b1a526f7e7
Aleksandr Rogalevich <sasa13481@gmail.com>
2023-09-26 18:37:27 +0300

    Рассмотрели сортировки.  Не дописана быстрая сортировка.

---
+++ Sorting/Sorting.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+void quickSort(int* mas, int left, int right) {
+	if (left >= right) {
+		return;
+	}
+	int pivot = left+1;
+	for (int i = left; i < right; i++) {
+		if (mas[i] < mas[left]) {
+			swap(mas[i], mas[pivot+1]);
+			pivot++;
+		}
+	}
+	swap(mas[0], mas[pivot]);
+	
+	quickSort(mas, left, pivot);
+	quickSort(mas, pivot, right);
+}
+int main() {
+	const int size = 10;
+	int mas[size];
+	
+	for (int i = 0; i < size; i++) {
+		//mas[i] = i + 1;
+		mas[i] = rand() % 10;
+		cout << "mas[" << i << "] = " << mas[i] << endl;
+	}
+	
+	//сортировка выбором
+	for (int i = 0; i < size-1; i++) {
+		int imin = i;
+		for (int k = i + 1; k < size; k++) {
+			if (mas[k] < mas[imin]) {
+				imin = k;
+			}
+		}
+		swap(mas[i], mas[imin]);
+	}
+	//Сортировка вставками
+	for (int i = 1; i < size; i++) {
+		for (int k = i; k > 0; k--) {
+			if (mas[k] < mas[k - 1]) {
+				swap(mas[k], mas[k - 1]);
+			}
+		}
+	}
+	quickSort(mas, 0, size);
+}
