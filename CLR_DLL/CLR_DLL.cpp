43b48879943140b3e427af40eddc5b0f6a4829a9
Aleksandr Rogalevich <sasa13481@gmail.com>
2024-02-23 17:04:06 +0300

    Рассмотрели меню и библиотеки

---
+++ CLR_DLL/CLR_DLL.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+
+#include "CLR_DLL.h"
+
+using namespace System;
+
+namespace CLRDLL {
+	public ref class Class1
+	{
+		// TODO: Добавьте сюда свои методы для этого класса.
+	public:
+		double sum(double a, double b) {
+			return a + b;
+		}
+	};
+}
